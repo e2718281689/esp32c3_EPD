84d987cb1d2bb80e1582322945f617727d785066
e2718281689 <2132906379@qq.com>
2022-06-25 21:48:35 +0800

    wav

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -13,47 +13,26 @@
 #include "web_serve.h"
 #include "EPD.h"
 #include "mqtt.h"
-
-#include "esp_console.h"
-#include <dirent.h>
-#include "argtable3/argtable3.h"
 #include "EPD_console.h"
+
 #define  path "/sdcard"
 
-//static struct {
-//    struct arg_int *port;
-//    struct arg_lit *all;
-//    struct arg_lit *list;
-//    struct arg_lit  *ls;
-//    struct arg_end *end;
-//} file_ls_args;
-//
-//static int file_ls_cmd(int argc, char **argv)
-//{
-//    int nerrors = arg_parse(argc, argv, (void **)&file_ls_args);
-//    if (nerrors != 0) {
-//        arg_print_errors(stderr, file_ls_args.end, argv[0]);
-//        return 0;
-//    }
-//    if(file_ls_args.all->count>0)
-//    {
-//        printf("file_ls_args.all\n");
-//    }
-//    if(file_ls_args.list->count>0)
-//    {
-//        printf("file_ls_args.list\n");
-//    }
-//    if(file_ls_args.port->count>0)
-//    {
-//        int chip_addr = file_ls_args.port->ival[0];
-//        printf("%d \n",chip_addr);
-//    }
-//    if(file_ls_args.ls->count>0)
-//    {
-//        printf("file_ls_args.ls\n");
-//    }
-//    return 0;
-//}
+static struct {
+     u8_t wav_id[4];
+     u8_t wav_Size[4];
+     u8_t wav_Type[4];
+
+     u8_t wav_fmt[4];
+     u8_t wav_Size1[16];
+     u8_t wav_AudioFormat[2];
+     u8_t wav_NumChannels[2];
+     u8_t wav_SampleRate[4];
+     u8_t wav_ByteRate[4];
+     u8_t wav_BlockAlign[2];
+     u8_t wav_BitsPerSample[2];
+
+} wav_date;
+
 void app_main(void)
 {
 //    ESP_ERROR_CHECK(nvs_flash_init());
@@ -64,46 +43,30 @@ void app_main(void)
 //    html_int();
 //    mqtt_app_start();
     mount_sdcard();
-////
-////    DIR *d; //声明一个句柄
-////    struct dirent *file; //readdir函数的返回值就存放在这个结构体中
-////    struct stat sb;
-////    if(!(d = opendir(path)))
-////    {
-////      printf("error opendir %s!!!\n",path);
-////    }
-////    while((file = readdir(d)) != NULL)
-////   {
-////       printf("%s \n",file->d_name);
-////   }
-//
-//    esp_console_repl_t *repl = NULL;
-//    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-//    repl_config.prompt = "EPD_sss>";
-//
-////    i2cconfig_args.port = arg_int0(NULL, "port", "<0|1>", "Set the I2C bus port number");
-////    i2cconfig_args.freq = arg_int0(NULL, "freq", "<Hz>", "Set the frequency(Hz) of I2C bus");
-////    i2cconfig_args.sda = arg_int1(NULL, "sda", "<gpio>", "Set the gpio for I2C SDA");
-////    i2cconfig_args.scl = arg_int1(NULL, "scl", "<gpio>", "Set the gpio for I2C SCL");
-////    i2cconfig_args.end = arg_end(2);
-//
-//    file_ls_args.port= arg_int0("c", "chip", "<chip_addr>", "Specify the address of the chip on that bus");
-//    file_ls_args.all = arg_litn("a","all",0,1,"Show All");
-//    file_ls_args.list= arg_litn("l","list",0,2,"Show list");
-//    file_ls_args.ls= arg_litn(NULL,NULL,0,1,"Show ls");
-//
-//    file_ls_args.end = arg_end(2);
-//    const esp_console_cmd_t file_cd_cmd = {
-//            .command = "ls",
-//            .help = "Config I2C bus",
-//            .hint = NULL,
-//            .func = &file_ls_cmd,
-//            .argtable = &file_ls_args
-//    };
-//
-//
-//    esp_console_dev_uart_config_t uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-//    ESP_ERROR_CHECK(esp_console_new_repl_uart(&uart_config, &repl_config, &repl));
-//    ESP_ERROR_CHECK(esp_console_start_repl(repl));
-    void EPD_console_init();
+
+    DIR *d; //声明一个句柄
+    struct dirent *file; //readdir函数的返回值就存放在这个结构体中
+    struct stat sb;
+    if(!(d = opendir(path)))
+    {
+      printf("error opendir %s!!!\n",path);
+    }
+    while((file = readdir(d)) != NULL)
+   {
+       printf("%s \n",file->d_name);
+   }
+    FILE *file_111;
+    char xxx[4];
+
+    file_111=fopen("/sdcard/123QWE.WAV","r");
+    for (int j = 0; j < 10; ++j)
+    {
+        fread(xxx,1,4,file_111);
+        for (int i = 0; i < 4; ++i)
+        {
+            printf("%c ",xxx[i]);
+        }
+        printf("\n");
+    }
+
 }
